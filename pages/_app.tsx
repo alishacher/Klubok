31346ee1384d137d3ea45900e87bf0593fa24611
calina <calina@scand.com>
2023-05-30 12:05:54 +0300

    Version problem resolved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 // _app.tsx
-
+import { Session } from "next-auth";
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-  );
+const App = ({ Component, pageProps }: AppProps<{session: Session}>) => {
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
 
 export default App;
