f38361af0b8a9249d7a7f61b33a15df67d81169a
calina <calina@scand.com>
2023-05-31 16:55:54 +0300

    Fixed db connection

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,20 +1,12 @@
-import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 export default async function handle(req, res) {
-    const { title, content,
-    authorEmail
-    } = req.body;
-
-    const session = await getSession({ });
-    // const authorEmail = session?.user?.email;
+    const { title, content, authorEmail} = req.body;
 
     const result = await prisma.post.create({
         data: {
             title: title,
-
             content: content,
-
             author: { connect: { email: authorEmail } },
         },
     });
