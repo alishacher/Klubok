2e315c9fbf109e35a90dda95494faab9f93a73cb
calina <calina@scand.com>
2023-05-31 16:49:22 +0300

    fix 13.6.4

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,20 +2,24 @@ import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 export default async function handle(req, res) {
-    const { title, content} = req.body;
-    alert('hi');
-    alert(req);
-    const session = await getSession({ });
-    const authorEmail = session?.user?.email;
+    try {
+        const { title, content} = req.body;
+        alert('hi');
+        alert(req);
+        const session = await getSession({ });
+        const authorEmail = session?.user?.email;
 
-    const result = await prisma.post.create({
-        data: {
-            title: title,
+        const result = await prisma.post.create({
+            data: {
+                title: title,
 
-            content: content,
+                content: content,
 
-            author: { connect: { email: authorEmail } },
-        },
-    });
-    res.json(result);
+                author: { connect: { email: authorEmail } },
+            },
+        });
+        res.json(result);
+    } catch (e) {
+        console.log(e)
+    }
 }
