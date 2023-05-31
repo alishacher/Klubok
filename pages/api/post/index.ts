5bec3b74940149f207fe662afd02c393d4fd497f
calina <calina@scand.com>
2023-05-31 16:24:50 +0300

    fix 13.6.4

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -3,7 +3,8 @@ import prisma from '../../../lib/prisma';
 
 export default async function handle(req, res) {
     const { title, content} = req.body;
-
+    alert('hi');
+    alert(req);
     const session = await getSession({ });
     const authorEmail = session?.user?.email;
 
