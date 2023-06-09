11cc61be776692f292ff91bac7968cd3fd077630
alishacher <alishacher@gmail.com>
2023-06-09 08:38:42 +0300

    fix 16.1.5

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "@/styles/Footer.module.scss";
 const Footer: React.FC = () => {
     return (
         <footer className={styles.footer_container}>
-          Created by <a href='https://github.com/alishacher'>alishacher</a>
+          Created by <a target='_blank' href='https://github.com/alishacher'>alishacher</a>
         </footer>
     );
 };
