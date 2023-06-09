54a354f1a26b3d9cf69a8f6a61496d4b88771a54
alishacher <alishacher@gmail.com>
2023-06-09 08:33:59 +0300

    fix 16.1.5

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,75 +5,11 @@ import { signOut, useSession } from 'next-auth/react';
 import styles from "@/styles/Header.module.scss";
 
 const Header: React.FC = () => {
+    const { data: session } = useSession();
     const router = useRouter();
+
     const isActive: (pathname: string) => boolean = (pathname) =>
         router.pathname === pathname;
-
-    const { data: session, status } = useSession();
-
-    let left = (
-        <div className="left">
-            <Link href="/">
-                <a className="bold" data-active={isActive('/')}>
-                    Community
-                </a>
-            </Link>
-        </div>
-    );
-
-    let right = null;
-
-    if (status === 'loading') {
-        left = (
-            <div className="left">
-                <Link href="/">
-                    <a className="bold" data-active={isActive('/')}>
-                        Community
-                    </a>
-                </Link>
-            </div>
-        );
-    }
-
-    if (!session) {
-        right = (
-            <div className="right">
-                <Link href="/api/auth/signin">
-                    <a data-active={isActive('/signup')}>Log in</a>
-                </Link>
-            </div>
-        );
-    }
-
-    if (session) {
-        left = (
-            <div className="left">
-                <Link href="/">
-                    <a className="bold" data-active={isActive('/')}>
-                        Community
-                    </a>
-                </Link>
-                <Link href="/drafts">
-                    <a data-active={isActive('/drafts')}>My drafts</a>
-                </Link>
-            </div>
-        );
-        right = (
-            <div className="right">
-                <p>
-                    {session.user.name} ({session.user.email})
-                </p>
-                <Link href="/create">
-                    <button>
-                        <a>New draft</a>
-                    </button>
-                </Link>
-                <button onClick={() => signOut()}>
-                    <a>Log out</a>
-                </button>
-            </div>
-        );
-    }
 
     return (
       <header className={styles.header_container}>
