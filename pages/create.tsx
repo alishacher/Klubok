1d1b9462f5ba4042d547507e524e6053444f2e62
alishacher <alishacher@gmail.com>
2023-06-09 10:49:00 +0300

    16.2.2

--- pages/create.tsx
+++ pages/create.tsx
@@ -32,6 +32,7 @@ const Draft: React.FC = () => {
             <div>
                 <form onSubmit={submitData}>
                     <input
+                        className={styles.draft_input}
                         autoFocus
                         onChange={(e) => setTitle(e.target.value)}
                         placeholder="Title"
@@ -39,6 +40,7 @@ const Draft: React.FC = () => {
                         value={title}
                     />
                     <textarea
+                        className={styles.draft_textarea}
                         cols={50}
                         onChange={(e) => setContent(e.target.value)}
                         placeholder="Content"
@@ -51,34 +53,6 @@ const Draft: React.FC = () => {
                     </a>
                 </form>
             </div>
-            <style jsx>{`
-        .page {
-          background: var(--geist-background);
-          padding: 3rem;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        input[type='text'],
-        textarea {
-          width: 100%;
-          padding: 0.5rem;
-          margin: 0.5rem 0;
-          border-radius: 0.25rem;
-          border: 0.125rem solid rgba(0, 0, 0, 0.2);
-        }
-
-        input[type='submit'] {
-          background: #ececec;
-          border: 0;
-          padding: 1rem 2rem;
-        }
-
-        .back {
-          margin-left: 1rem;
-        }
-      `}</style>
         </Layout>
     );
 };
