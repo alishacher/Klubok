6ca146040fe2bf9b95f4e432228a78b8d503c9ce
alishacher <alishacher@gmail.com>
2023-06-09 11:02:20 +0300

    16.2.5

--- pages/create.tsx
+++ pages/create.tsx
@@ -29,28 +29,30 @@ const Draft: React.FC = () => {
     return (
         <Layout>
             <h1 className={styles.title}>✎ New Draft</h1>
-            <form onSubmit={submitData}>
-                <input
-                    className={styles.draft_input}
-                    autoFocus
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="Title"
-                    type="text"
-                    value={title}
-                />
-                <textarea
-                    className={styles.draft_textarea}
-                    cols={50}
-                    onChange={(e) => setContent(e.target.value)}
-                    placeholder="Content"
-                    rows={8}
-                    value={content}
-                />
-                <input disabled={!content || !title} type="submit" value="Create" />
-                <a className="back" href="#" onClick={() => Router.push('/')}>
-                    or Cancel
-                </a>
-            </form>
+            <main className={styles.draft_wrapper}>
+                <form onSubmit={submitData}>
+                    <input
+                        className={styles.draft_input}
+                        autoFocus
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Title"
+                        type="text"
+                        value={title}
+                    />
+                    <textarea
+                        className={styles.draft_textarea}
+                        cols={50}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="Content"
+                        rows={8}
+                        value={content}
+                    />
+                    <input disabled={!content || !title} type="submit" value="Create" />
+                    <a className="back" href="#" onClick={() => Router.push('/')}>
+                        or Cancel
+                    </a>
+                </form>
+            </main>
         </Layout>
     );
 };
