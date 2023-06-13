58376eae37de624acea512ae9cde1c1c0489c91d
calina <calina@scand.com>
2023-06-13 11:31:56 +0300

    fix 16.2.5

--- pages/api/counter/[id].ts
+++ pages/api/counter/[id].ts
@@ -3,7 +3,7 @@ import prisma from '../../../lib/prisma';
 export default async function handle(req, res) {
     const postId = req.query.postID;
     if (req.method === "PATCH") {
-        const counter = +req.data.counter;
+        const counter = +req.body.counter;
         const post = await prisma.post.update({
             where: {id: postId},
             data: {counter: counter},
