f518ac870be8f13ca8fa8ab48ffbf3e9db60b511
calina <calina@scand.com>
2023-06-13 11:15:22 +0300

    fix 16.2.1

--- pages/api/counter/[id].ts
+++ pages/api/counter/[id].ts
@@ -2,10 +2,17 @@ import prisma from '../../../lib/prisma';
 
 export default async function handle(req, res) {
     const postId = req.query.id;
-    const counter = req.data.counter;
-    const post = await prisma.post.update({
-        where: { id: postId },
-        data: { counter: counter },
-    });
-    res.json(post);
+    if (req.method === "PATCH") {
+
+        const counter = req.data.counter;
+        const post = await prisma.post.update({
+            where: {id: postId},
+            data: {counter: counter},
+        });
+        res.json(post);
+    } else {
+        throw new Error(
+            `The HTTP ${req.method} method is not supported at this route.`,
+        );
+    }
 }
